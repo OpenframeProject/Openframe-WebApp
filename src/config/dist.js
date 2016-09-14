6bed22df3d210043d042b0f777ede05feb032eaf
Jonathan Wohl <jon@jonwohl.com>
2016-09-14 13:05:45 -0400

    Update to use dev server API

--- src/config/dist.js
+++ src/config/dist.js
@@ -4,7 +4,7 @@ import baseConfig from './base';
 
 let config = {
   appEnv: 'dist',  // feel free to remove the appEnv property here
-  apiBase: 'localhost:3000/api/'
+  apiBase: 'dev.openframe.io:3000/api/'
 };
 
 export default Object.freeze(Object.assign({}, baseConfig, config));
