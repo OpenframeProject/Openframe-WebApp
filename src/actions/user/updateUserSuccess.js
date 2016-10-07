6ab196d677da519691587e9b45d325f6f8c960ec
Jonathan Wohl <jon@jonwohl.com>
2016-10-07 10:30:48 -0500

    Don't redirect on profile save

--- src/actions/user/updateUserSuccess.js
+++ src/actions/user/updateUserSuccess.js
@@ -1,13 +1,10 @@
 import { normalize } from 'normalizr';
-import { browserHistory } from 'react-router';
 import {UPDATE_USER_SUCCESS} from './../const';
 import * as schema from '../schema';
 
 const updateNoticeBanner = require('../ui/updateNoticeBanner.js');
 
 module.exports = function(response, notice) {
-  browserHistory.push('/');
-
   return dispatch => {
     dispatch({
       type: UPDATE_USER_SUCCESS,
