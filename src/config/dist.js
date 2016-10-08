fa1249c34bc6f8dd07b83f6146d1f00da3b0fb69
Jonathan Wohl <jon@jonwohl.com>
2016-10-07 22:49:58 -0500

    Use HTTPS

--- src/config/dist.js
+++ src/config/dist.js
@@ -4,7 +4,7 @@ import baseConfig from './base';
 
 let config = {
   appEnv: 'dist',  // feel free to remove the appEnv property here
-  apiBase: 'http://dev.openframe.io:3000/api/'
+  apiBase: 'https://dev.openframe.io:3000/api/'
 };
 
 export default Object.freeze(Object.assign({}, baseConfig, config));
