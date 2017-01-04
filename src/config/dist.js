6f39c871edd5ac6dab0f417681a5282dc03fc4d7
Jonathan Wohl <jon@jonwohl.com>
2017-01-04 13:14:20 -0500

    Use api.openframe.io server

--- src/config/dist.js
+++ src/config/dist.js
@@ -4,7 +4,7 @@ import baseConfig from './base';
 
 let config = {
   appEnv: 'dist',  // feel free to remove the appEnv property here
-  apiBase: 'https://dev.openframe.io/api/',
+  apiBase: 'https://api.openframe.io/v0/',
   crossStorageRules: [
     {
         origin: /editor\.thebookofshaders\.com$/,
